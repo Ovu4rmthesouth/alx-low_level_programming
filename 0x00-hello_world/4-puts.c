570025f2ceedd36adaf48cb71a12c81c40a83ad0
“Ogheneovu” <Ogheneovu.kwame@gmail.com>
2022-09-08 15:48:25 -0700

    update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
+
 /**
- *  * main - entry point
- *   * Return: 0
- *    */
+ *  * main - printing 
+ *   *
+ *    * Return: Always 0 (successful)
+ *     */
 int main(void)
 {
 		puts("\"Programming is like building a multilingual puzzle");
