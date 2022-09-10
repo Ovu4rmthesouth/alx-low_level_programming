9268ec4c258888696cfd2e35ee2d952dada1220f
“Ogheneovu” <Ogheneovu.kwame@gmail.com>
2022-09-10 12:30:29 -0700

    advanced

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -9,13 +9,11 @@ int c = 0;
 int f_d;
 int m_d;
 int l_d;
-
 while (c <= 999)
 {
 f_d = (c / 100 + '0');
 m_d = (c / 10 % 10 + '0');
 l_d = (c % 10 + '0');
-
 if ((f_d < m_d) && (m_d < l_d))
 {
 putchar(f_d);
